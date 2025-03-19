ade44925c72ca63a0cfe6c1d243e59629b7f5afd
이진석 <ljs@whisker.kr>
2025-03-19 14:00:26 +0900

    [Main page] 생성 및 gradle 추가
    Main Page 참조 : jpashop
    thymeleaf, validation gradle 추가

--- src/test/java/com/hertz/shoppingMall/DataBaseConnectionTest.java
+++ src/test/java/com/hertz/shoppingMall/DataBaseConnectionTest.java
@@ -1,12 +1,11 @@
 package com.hertz.shoppingMall;
 
-import com.hertz.shoppingMall.member.Member;
-import com.hertz.shoppingMall.member.Repository.MemberRepository;
+import com.hertz.shoppingMall.member.model.MemberVO;
+import com.hertz.shoppingMall.member.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
@@ -23,15 +22,15 @@ public class DataBaseConnectionTest {
     @DisplayName("회원 저장 및 조회 테스트")
     void saveAndFindMember(){
         // Given (테스트 데이터 준비)
-        Member member = new Member();
+        MemberVO member = new MemberVO();
         member.setName("Test1");
         member.setEmail("test@example.com");
 
         // When (저장)
-        Member saveMember = memberRepository.save(member);
+        MemberVO saveMember = memberRepository.save(member);
 
         // Then (검증)
-        Member foundMember = memberRepository.findById(saveMember.getId()).orElse(null);
+        MemberVO foundMember = memberRepository.findById(saveMember.getId()).orElse(null);
         assertThat(foundMember).isNotNull();
         assertThat(foundMember.getName()).isEqualTo("Test1");
         assertThat(foundMember.getEmail()).isEqualTo("test@example.com");
