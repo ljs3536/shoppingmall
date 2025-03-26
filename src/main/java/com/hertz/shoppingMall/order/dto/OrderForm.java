1c14d32db08d6c223d01f09cd85ea7180455e357
이진석 <ljs@whisker.kr>
2025-03-26 14:42:45 +0900

    주문 Validation 추가 및 상태 Enum 추가
    OrderForm 수정
    OrderForm Validation 추가
    Order Model 수정

--- src/main/java/com/hertz/shoppingMall/order/dto/OrderForm.java
+++ src/main/java/com/hertz/shoppingMall/order/dto/OrderForm.java
@@ -1,5 +1,8 @@
 package com.hertz.shoppingMall.order.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -9,8 +12,17 @@ import java.util.List;
 public class OrderForm {
 
     private List<OrderItemDto> orderItems = new ArrayList<>();
+
+    // 수령인
+    @NotBlank(message = "수령인 정보는 필수입니다.")
     private String recipient;
+    // 배송자 연락처
+    @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "올바른 전화번호 형식이 아닙니다")
     private String phoneNumber;
+    // 배송 주소
+    @NotBlank(message = "배송 주소는 필수입니다.")
     private String address;
+    // 주문 요청사항
+    @Size(max = 500, message = "주문 요청사항은 500자를 초과할 수 없습니다")
     private String orderRequest;
 }
