374924634a5ecea52eae6a00376c6f4e1db0abab
이진석 <ljs@whisker.kr>
2025-03-27 16:54:06 +0900

    재고 감소 메서드 추가
    상세보기에서 주문 시 수량 변수도 넘기도록 수정

--- src/main/java/com/hertz/shoppingMall/product/model/Product.java
+++ src/main/java/com/hertz/shoppingMall/product/model/Product.java
@@ -3,6 +3,7 @@ package com.hertz.shoppingMall.product.model;
 
 import com.hertz.shoppingMall.config.jpa.BaseDateEntity;
 import com.hertz.shoppingMall.member.model.Member;
+import com.hertz.shoppingMall.utils.exception.custom.NotEnoughStockException;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
@@ -35,4 +36,13 @@ public class Product extends BaseDateEntity {
     @ManyToOne
     @JoinColumn(name = "modified_by") // 마지막으로 수정한 회원
     private Member modifiedBy;
+
+    // 재고 감소 메서드
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
