d1e1fff883e6bd925f7a158387c7a216f981982b
이진석 <ljs@whisker.kr>
2025-04-18 09:02:41 +0900

    flask API 설정 수정

--- src/main/java/com/hertz/shoppingMall/config/webclient/WebClientConfig.java
+++ src/main/java/com/hertz/shoppingMall/config/webclient/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.hertz.shoppingMall.config.webclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${flask.base-url}")
+    private String flaskBaseUrl;
+
     @Bean
     public WebClient webClient(){
         return WebClient.builder()
-                .baseUrl("http://localhost:5000") // Flask 서버 주소
+                .baseUrl(flaskBaseUrl)
                 .build();
     }
 }
