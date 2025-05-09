4ebb2ed806eefacb456751c783fca9d77c3a1e34
이진석 <ljs@whisker.kr>
2025-05-09 09:12:14 +0900

    저장된 알림 데이터 세팅 및 알림 확인, 삭제 기능 추가

--- src/main/java/com/hertz/shoppingMall/alarm/repository/AlarmRepository.java
+++ src/main/java/com/hertz/shoppingMall/alarm/repository/AlarmRepository.java
@@ -3,5 +3,9 @@ package com.hertz.shoppingMall.alarm.repository;
 import com.hertz.shoppingMall.alarm.model.Alarm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+import java.util.Collection;
+import java.util.Optional;
+
+public interface AlarmRepository extends JpaRepository<Alarm, Long>, AlarmRepositoryCustom {
+    Optional<Alarm> findByIdAndTargetMemberId(Long id, Long memberId);
 }
